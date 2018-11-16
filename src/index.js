962c3bfaab51a4018c9a2bf01ba44891ee99a399
KarinaDavtyan <k.davtyan27@gmail.com>
2018-11-16 12:23:58 +0100

    feat: add visualisation voyager middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import Koa from 'koa';
 import { ApolloServer } from 'apollo-server-koa';
-import { importSchema } from 'graphql-import'
+import { importSchema } from 'graphql-import';
+import KoaRouter from 'koa-router';
+import { koa as voyagerMiddleware } from 'graphql-voyager/middleware';
 
 import types from './graphql/types/main.graphql';
 import resolvers from './graphql/resolvers/main.js';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const host = 'localhost';
 
 const typeDefs = importSchema(types)
 
 const server = new ApolloServer({ typeDefs, resolvers });
 const app = new Koa();
+const router = new KoaRouter();
+
+//visualisation middleware
+router.all('/voyager', voyagerMiddleware({
+  endpointUrl: '/graphql'
+}));
+
+app.use(router.routes());
+
 server.applyMiddleware({ app });
 
 app.listen(port, host, () =>
